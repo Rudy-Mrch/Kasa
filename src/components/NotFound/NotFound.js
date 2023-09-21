96dcd338b1c6f42f60b34e81654408569b372a40
Rudy-Mrch <rudy.marache@gmail.com>
2023-09-21 18:12:01 +0200

    initialisation du projet Kasa

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page non trouvée</h1>
+      <p>Désolé, la page que vous recherchez n'existe pas.</p>
+    </div>
+  );
+};
+
+export default NotFound;
