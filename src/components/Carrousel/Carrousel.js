184ec3dc85c6da105009e19a61e8a8f6b80b6768
Rudy-Mrch <rudy.marache@gmail.com>
2023-10-16 19:44:53 +0200

    correction soutenance

--- src/components/Carrousel/Carrousel.js
+++ src/components/Carrousel/Carrousel.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 const Carrousel = ({ slides }) => {
   const slidesLength = slides.length;
   const [place, setPlace] = useState(0);
+  const showArrows = slidesLength > 1;
 
   const backSlide = () => {
     setPlace(place === 0 ? slidesLength - 1 : place - 1);
@@ -13,37 +14,42 @@ const Carrousel = ({ slides }) => {
     setPlace(place === slidesLength - 1 ? 0 : place + 1);
   };
 
-return (
-  <div className='carrousel-container'>
-    {slides.map((slide, index) => (
-      <div key={index} className="carrouselPix">
-        {index === place && (
-          <div>
-            <img
-              className='carrousel-container__slide' 
-              src={slide}
-              alt="photos des appartements"
-            />
-            <img
-              className='carrousel-container__arrowBack'
-              src="/pictures/arrowBack.png"
-              alt='fleche precedent'
-              onClick={backSlide}
-            />
-            <img
-              className='carrousel-container__arrowNext'
-              src="/pictures/arrowNext.png"
-              alt="fleche suivante"
-              onClick={nextSlide}
-            />
-          </div>
-        )}
-        {index === place && (
-          <span className='carrousel-container__number'>{place + 1}/{slidesLength}</span>
-        )}
-      </div>
-    ))}
-  </div>
-);
-        }
-export default Carrousel
+  return (
+    <div className='carrousel-container'>
+      {slides.map((slide, index) => (
+        <div key={index} className="carrouselPix">
+          {index === place && (
+            <div>
+              <img
+                className='carrousel-container__slide' 
+                src={slide}
+                alt="photos des appartements"
+              />
+              {showArrows && (
+                <>
+                  <img
+                    className='carrousel-container__arrowBack'
+                    src="/pictures/arrowBack.png"
+                    alt='fleche precedent'
+                    onClick={backSlide}
+                  />
+                  <img
+                    className='carrousel-container__arrowNext'
+                    src="/pictures/arrowNext.png"
+                    alt="fleche suivante"
+                    onClick={nextSlide}
+                  />
+                </>
+              )}
+            </div>
+          )}
+          {index === place && (
+            <span className='carrousel-container__number'>{place + 1}/{slidesLength}</span>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Carrousel;
